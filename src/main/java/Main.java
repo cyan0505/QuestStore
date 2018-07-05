a315e2465e1057f27f9250f51747741d987eb4d9
Cyan <dawid.grygier5@gmail.com>
2018-07-05 11:28:25 +0200

    test app frist build

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.sql.SQLException;
+
+public class Main {
+
+    private static View view;
+    private static Controller controller;
+
+    public static void main(String[] args) {
+        view = new View();
+        controller = new Controller();
+
+        Boolean option = true;
+        while (option) {
+            view.printMenu();
+            Integer answer = view.getAnswerAsInt("Choose option: ");
+            try {
+                option = controller.handleMenu(answer);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/View.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class View {
+
+    private Scanner scanner;
+
+    public View() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void printMenu() {
+        List<String> menu = Arrays.asList("Insert row to table.", "delete row", "select * from table",
+                "Show full name and phone number of applicants with given email", "Add applicant and show his row", "Update applicants phone number, and show it",
+                "Delete applicants with given email", "Advance search");
+        System.out.printf("%nWhat would you like to do:%n");
+        int index = 1;
+        for (String menuItem : menu) {
+            System.out.printf("    (%d) %s%n", index, menuItem);
+            index++;
+        }
+        System.out.println("    (0) Exit");
+    }
+
+    public String getAnswerAsString(String question) {
+        System.out.print(question);
+        return scanner.nextLine();
+    }
+
+    public Integer getAnswerAsInt(String question) {
+        Integer input = null;
+        while (input == null) {
+            System.out.print(question);
+            try {
+                input = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.err.println("Type integer!");
+            }
+        }
+        scanner.nextLine();
+        return input;
+    }
+}
