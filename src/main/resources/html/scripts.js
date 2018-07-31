5edcd3e9ee959934d3562716860f015d95936d5d
Cyan <dawid.grygier5@gmail.com>
2018-07-31 10:23:25 +0200

    Final front commit

--- src/main/resources/html/scripts.js
+++ src/main/resources/html/scripts.js
@@ -90,4 +90,10 @@ function myFunction() {
     }
 }
 
-
+$('head style[type="text/css"]').attr('type', 'text/less');
+less.refreshStyles();
+window.randomize = function() {
+	$('.radial-progress').attr('data-progress', Math.floor(Math.random() * 100));
+}
+setTimeout(window.randomize, 200);
+$('.radial-progress').click(window.randomize);
