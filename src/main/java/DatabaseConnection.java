9e1a957fe9a6c5346cc2cdaa229a1dac4cc940f8
Szwajcii <d.szwajkos@gmail.com>
2018-07-31 10:43:15 +0200

    Implement methods to open and close connection with database.

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,9 +1,12 @@
-
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnection {
 
-
+    private Connection c;
+    private Statement stmt;
 
     private static DatabaseConnection instance = new DatabaseConnection();
 
@@ -13,6 +16,24 @@ public class DatabaseConnection {
 
     public static DatabaseConnection getInstance() {
         return instance;
+    }
+
+    public void getConnetion() {
+        try {
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/QuestStore", "postgres", "yakuza06"); // set user and password
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            c.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
