35aa09586883649e6faebf63e92ffb908c924522
Riczard <riczardkiller@gmail.com>
2018-07-31 14:43:41 +0200

    Change folder structure, add connect to server, add index.html as static

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8800), 0);
+        server.createContext("/static", new Controller());
+        server.setExecutor(null);
+        server.start();
+    }
+}
