1370dd70127bcb4eaf435a798a9c0913e0dd1a54
Riczard <riczardkiller@gmail.com>
2018-08-01 13:20:56 +0200

    Add main page to codecooler

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,7 @@ public class App {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8800), 0);
         server.createContext("/static", new Controller());
+        server.createContext("/codecooler", new CodecoolerController());
         server.setExecutor(null);
         server.start();
     }
--- src/main/java/Controller/CodecoolerController.java
+++ src/main/java/Controller/CodecoolerController.java
@@ -2,6 +2,7 @@ package Controller;
 
 
 import java.io.*;
+import java.net.URI;
 import java.net.URLDecoder;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -18,6 +19,43 @@ import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
+import static Controller.Controller.parseUri;
 
-public class CodecoolerController {
+
+public class CodecoolerController implements HttpHandler{
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+        String method = httpExchange.getRequestMethod();
+        String response = "";
+        URI uri = httpExchange.getRequestURI();
+        String[] parsedUri = parseUri(uri.toString());
+
+        if(method.equals("GET")) {
+            if(parsedUri[1].equals("codecooler")){
+                showCodecoolerMainPage();
+            }
+
+        }
+
+        if(method.equals("POST")) {
+
+
+        }
+
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+
+    }
+
+    private String showCodecoolerMainPage(){
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/codecoolerMainPage.twig");
+        JtwigModel model = JtwigModel.newModel();
+
+        //model.with("studentList", studentsList);
+
+        return template.render(model);
+    }
 }
