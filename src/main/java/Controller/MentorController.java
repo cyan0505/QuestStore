3397e524625ad6a3a75e0b50cf2b013270a1de03
Szwajcii <d.szwajkos@gmail.com>
2018-08-01 09:42:33 +0200

    Implement new controller class.

--- src/main/java/Controller/MentorController.java
+++ src/main/java/Controller/MentorController.java
@@ -1,11 +1,152 @@
 package Controller;
 
+import java.io.*;
+import java.net.URLDecoder;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import DAO.CodecoolerDAO;
+import DAO.UserDAO;
+import Model.Codecooler;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 public class MentorController implements HttpHandler {
 
+    private UserDAO userDao = new UserDAO();
 
 
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+        String method = httpExchange.getRequestMethod();
+        String response = "";
+
+        if(method.equals("GET")) {
+
+            String[] uri = parseFromData(httpExchange.getRequestURI().toString());
+
+            if(uri.length >= 3) {
+
+                if(uri[2].equals("delete")) {
+
+                    int studentId = Integer.parseInt(uri[3]);
+
+                    delete(studentId);
+
+
+                }
+
+                if(uri[2].equals("add")) {
+
+                    JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/addStudent.twig");
+
+                    JtwigModel model = JtwigModel.newModel();
+
+                    response = template.render(model);
+
+                }
+
+                if(uri[2].equals("edit")) {
+
+                    JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/editStudent.twig");
+
+                    JtwigModel model = JtwigModel.newModel();
+
+                    response = template.render(model);
+
+                }
+
+                else {
+                    redirectToLocation(httpExchange, "/main-mentor");
+                }
+
+            }
+
+            if(method.equals("POST")) {
+                InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody());
+                BufferedReader br = new BufferedReader(isr);
+                String formData = br.readLine();
+
+                Map inputs = parseStudentInfoFromData(formData);
+
+
+                String firstName = inputs.get("firstName").toString();
+                String lastName = inputs.get("lastName").toString();
+                String login = inputs.get("login").toString();
+                String password = inputs.get("pass").toString();
+                String email = inputs.get("email").toString();
+                String role = "codecooler";
+
+                Codecooler codecooler = new Codecooler(firstName, lastName, login, password, email, role);
+
+                try {
+                    userDao.addUser(codecooler);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+
+            httpExchange.sendResponseHeaders(200, response.length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+
+
+        }
+
+
+    }
+
+
+
+
+    private void delete(int id) {
+
+    }
+
+
+    private String[] parseFromData(String fromData) {
+        String[] pairs = fromData.split("/");
+        return pairs;
+    }
+
+
+    private Map<String, String> parseStudentInfoFromData(String fromData) throws UnsupportedEncodingException {
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = fromData.split("&");
+        for(String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            String value = new URLDecoder().decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+
+        return map;
+    }
+
+
+    private void redirectToLocation(HttpExchange exchange, String location) {
+
+        Headers responseHeaders = exchange.getResponseHeaders();
+        responseHeaders.add("Location", location);
+
+        try {
+            exchange.sendResponseHeaders(302, -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        exchange.close();
+
+    }
 
 
 
