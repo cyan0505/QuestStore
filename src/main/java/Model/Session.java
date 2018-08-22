2915e399b3af31f7e15b7ec5dc40e8505df209c1
Riczard <riczardkiller@gmail.com>
2018-08-22 21:30:28 +0200

    Add Session model

---
+++ src/main/java/Model/Session.java
@@ -0,0 +1,48 @@
+package Model;
+
+import java.net.HttpCookie;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Session {
+
+    private final UUID uuid;
+    private final String USER_LOGIN;
+    private static List<Session> connectionList = new ArrayList<>();
+
+
+    public Session(String USER_LOGIN) {
+        this.uuid = UUID.randomUUID();
+        this.USER_LOGIN = USER_LOGIN;
+        connectionList.add(this);
+    }
+
+    public static String getUSER_LOGIN(String uuid) {
+
+        for (Session session : connectionList){
+            if(session.uuid.equals(uuid)) return session.USER_LOGIN;
+        }
+        return null;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void removeSessionBy(String login){
+
+        for(Session session : connectionList){
+            if(session.USER_LOGIN.equals(login)){
+                connectionList.remove(session);
+                break;
+            }
+        }
+    }
+
+    public HttpCookie getCookie(){
+
+        return new HttpCookie("Session-id", this.uuid.toString());
+
+    }
+}
