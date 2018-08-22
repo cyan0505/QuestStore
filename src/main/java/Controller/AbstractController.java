23bed6f9a7bafcef2391f65f6f62c454cb6f3104
Riczard <riczardkiller@gmail.com>
2018-08-22 21:32:06 +0200

    Add abstract controller

--- src/main/java/Controller/AbstractController.java
+++ src/main/java/Controller/AbstractController.java
@@ -1,4 +1,71 @@
 package Controller;
 
-public class AbstractController {
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import helpers.MimeTypeResolver;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractController {
+
+    protected void sendFile(HttpExchange httpExchange, URL fileURL) throws IOException {
+        // get the file
+        File file = new File(fileURL.getFile());
+        // we need to find out the mime type of the file, see: https://en.wikipedia.org/wiki/Media_type
+        MimeTypeResolver resolver = new MimeTypeResolver(file);
+        String mime = resolver.getMimeType();
+
+        httpExchange.getResponseHeaders().set("Content-Type", mime);
+        httpExchange.sendResponseHeaders(200, 0);
+
+        OutputStream os = httpExchange.getResponseBody();
+
+        // send the file
+        FileInputStream fs = new FileInputStream(file);
+        final byte[] buffer = new byte[0x10000];
+        int count = 0;
+        while ((count = fs.read(buffer)) >= 0) {
+            os.write(buffer, 0, count);
+        }
+        os.close();
+    }
+
+    protected void send404(HttpExchange httpExchange) throws IOException {
+        String response = "404 (Not Found)\n";
+        httpExchange.sendResponseHeaders(404, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.toString().getBytes());
+        os.close();
+    }
+
+    public void redirectToLocation(HttpExchange exchange, String location) {
+        Headers responseHeaders = exchange.getResponseHeaders();
+        responseHeaders.add("Location", location);
+
+        try {
+            exchange.sendResponseHeaders(302, -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        exchange.close();
+    }
+
+    public Map<String, String> parseUserInfoFromData(String fromData) throws UnsupportedEncodingException {
+
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = fromData.split("&");
+
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            String value = new URLDecoder().decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+
+        return map;
+    }
+
 }
