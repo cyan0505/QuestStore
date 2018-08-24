4bd8e3c00a21dbbaecbf33bee9bd4e92d01ab057
Riczard <riczardkiller@gmail.com>
2018-08-25 00:11:50 +0200

    refactor

--- src/main/java/Controller/CodecoolerController.java
+++ src/main/java/Controller/CodecoolerController.java
@@ -8,116 +8,99 @@ import java.util.List;
 
 import DAO.ArtefactDAO;
 import DAO.CodecoolerDAO;
+import DAO.MentorDAO;
 import DAO.QuestDAO;
 import Model.Codecooler;
+import Model.Mentor;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import com.sun.org.apache.bcel.internal.classfile.Code;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
 import static Controller.Controller.parseUri;
 
 
-public class CodecoolerController implements HttpHandler{
+public class CodecoolerController extends AbstractController implements HttpHandler{
 
-    private CodecoolerDAO codecoolerDao = new CodecoolerDAO();
     private ArtefactDAO artefactDao = new ArtefactDAO();
     private QuestDAO questDao = new QuestDAO();
 
+
     @Override
-    public void handle(HttpExchange httpExchange) throws IOException {
+    public void handle(HttpExchange httpExchange) throws IOException{
 
         String method = httpExchange.getRequestMethod();
-        String response = "";
-        URI uri = httpExchange.getRequestURI();
-        String[] parsedUri = parseUri(uri.toString());
-        System.out.println(uri);
-
-        if(method.equals("GET")) {
-            response = chooseProperPage(parsedUri);
+        if(method.equals("POST")){
+            handlePost(httpExchange);
+        }else{
+            try{
+            handleSession(httpExchange);
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
         }
+    }
 
-        if(method.equals("POST")) {
-
-
-        }
-
-        httpExchange.sendResponseHeaders(200, response.length());
-        OutputStream os = httpExchange.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
+    private void handlePost(HttpExchange httpExchange){
 
     }
 
-    private String chooseProperPage(String[] parsedUri){
+    private void handleSession(HttpExchange httpExchange) throws IOException, SQLException{
+
+        String respone = chooseProperPage(httpExchange);
+        OutputStream os = httpExchange.getResponseBody();
+
+        if(respone == null){
+            send404(httpExchange);
+        } else{
+            sendReq(httpExchange, respone);
+        }
+    }
+
+    private String chooseProperPage(HttpExchange httpExchange) throws SQLException{
+        URI uri = httpExchange.getRequestURI();
+        String[] parsedUri = parseUri(uri.toString());
+
         String response="";
-        int mainPageIndex = 1;
-        int idIndex = 2;
-        int subPageIndex = 3;
-        try {
-            if (parsedUri.length == 3 && parsedUri[mainPageIndex].equals("codecooler")) {
-                response = getCodecoolerMainPage(parsedUri[idIndex]);
+
+        int subPageIndex = 2;
+        int subPageUriLength = 3;
+
+        String login = getLoginByCookie(httpExchange);
+        Codecooler codecooler =getCodecoolerByLogin(login);
+
+
+        if(parsedUri.length == subPageUriLength){
+
+            if(parsedUri[subPageIndex].equals("profile")){
+                response = renderPage(codecooler, "templates/codecoolerProfile.twig");
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("profile")) {
-                response = getCodecoolerProfile(parsedUri[idIndex]);
+            else if(parsedUri[subPageIndex].equals("contact")){
+                response = renderPage(codecooler, "templates/codecoolerContact.twig");
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("contact")){
-                response = getCodecoolerContact(parsedUri[idIndex]);
+            else if(parsedUri[subPageIndex].equals("store")){
+                response = getCodecoolerStore(codecooler);
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("store")){
-                response = getCodecoolerStore(parsedUri[idIndex]);
+            else if(parsedUri[subPageIndex].equals("quest")){
+                response = getCodecoolerQuest(codecooler);
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("quest")){
-                response = getCodecoolerQuest(parsedUri[idIndex]);
+            else if(parsedUri[subPageIndex].equals("inventory")){
+                response = renderPage(codecooler, "templates/codecoolerWallet.twig");
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("inventory")){
-                response = getCodecoolerInventory(parsedUri[idIndex]);
-            }
-        } catch (SQLException e){
-            e.printStackTrace();
+
+        } else{
+            response = renderPage(codecooler, "templates/codecoolerMainPage.twig");
         }
         return response;
     }
 
 
-    private String getCodecoolerMainPage(String id) throws SQLException{
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/codecoolerMainPage.twig");
-        JtwigModel model = JtwigModel.newModel();
 
-        Codecooler codecooler = codecoolerDao.getCodecooler(id);
-
-        model.with("codecooler", codecooler);
-
-        return template.render(model);
-    }
-
-    private String getCodecoolerProfile(String id) throws SQLException{
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/codecoolerProfile.twig");
-        JtwigModel model = JtwigModel.newModel();
-
-        Codecooler codecooler = codecoolerDao.getCodecooler(id);
-
-        model.with("codecooler", codecooler);
-
-        return template.render(model);
-    }
-
-    private String getCodecoolerContact(String id) throws SQLException{
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/codecoolerContact.twig");
-        JtwigModel model = JtwigModel.newModel();
-
-        Codecooler codecooler = codecoolerDao.getCodecooler(id);
-
-        model.with("codecooler", codecooler);
-
-        return template.render(model);
-    }
-
-    private String getCodecoolerStore(String id) throws SQLException{
+    private String getCodecoolerStore(Codecooler codecooler) throws SQLException{
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/codecoolerStore.twig");
         JtwigModel model = JtwigModel.newModel();
 
-        Codecooler codecooler = codecoolerDao.getCodecooler(id);
         List<List> listOfLists = artefactDao.getNestedArtifactList(artefactDao.getListOfArtifact());
 
         model.with("artifacts", listOfLists);
@@ -126,11 +109,10 @@ public class CodecoolerController implements HttpHandler{
         return template.render(model);
     }
 
-    private String getCodecoolerQuest(String id) throws SQLException{
+    private String getCodecoolerQuest(Codecooler codecooler) throws SQLException{
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/codecoolerQuest.twig");
         JtwigModel model = JtwigModel.newModel();
 
-        Codecooler codecooler = codecoolerDao.getCodecooler(id);
         List<List> listOfLists = questDao.getNestedQuestList(questDao.getListOfQuests());
 
         model.with("codecooler", codecooler);
@@ -140,15 +122,9 @@ public class CodecoolerController implements HttpHandler{
 
     }
 
-    private String getCodecoolerInventory(String id) throws SQLException{
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/codecoolerWallet.twig");
-        JtwigModel model = JtwigModel.newModel();
 
-        Codecooler codecooler = codecoolerDao.getCodecooler(id);
-
-        model.with("codecooler", codecooler);
-//        model.with("inventory", codecooler.getInventory().getArtifactList());
-
-        return template.render(model);
+    private Codecooler getCodecoolerByLogin(String login) throws SQLException {
+        CodecoolerDAO codecoolerDAO = new CodecoolerDAO();
+        return codecoolerDAO.getCodecooler(login);
     }
 }
