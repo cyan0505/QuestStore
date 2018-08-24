fcc68928646559fd1d61ced22abfc77a8c2ea3fd
Riczard <riczardkiller@gmail.com>
2018-08-25 00:13:00 +0200

    change name

--- src/main/java/Controller/MentorController.java
+++ src/main/java/Controller/MentorController.java
@@ -1,13 +1,11 @@
 package Controller;
 
-import java.io.*;
-import java.net.URI;
-import java.sql.SQLException;
-import java.util.List;
-
 import BuisnessLogic.Artifact;
 import BuisnessLogic.Quest;
-import DAO.*;
+import DAO.ArtefactDAO;
+import DAO.CodecoolerDAO;
+import DAO.MentorDAO;
+import DAO.QuestDAO;
 import Model.Codecooler;
 import Model.Mentor;
 import com.sun.net.httpserver.HttpExchange;
@@ -15,36 +13,49 @@ import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.List;
+
 import static Controller.Controller.parseUri;
 
-public class MentorController implements HttpHandler {
+public class MentorController extends AbstractController implements HttpHandler {
 
-    private UserDAO userDao = new UserDAO();
     private ArtefactDAO artefactDao = new ArtefactDAO();
     private QuestDAO questDao = new QuestDAO();
-    private MentorDAO mentorDao = new MentorDAO();
     private CodecoolerDAO codecoolerDao = new CodecoolerDAO();
-    private final int mainPageIndex = 1;
-    private final int idIndex = 2;
-    private final int subPageIndex = 3;
 
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
         String method = httpExchange.getRequestMethod();
-        String response = "";
+        if (method.equals("POST")) {
+            handlePost(httpExchange);
+        } else {
+
+            try {
+                handleSession(httpExchange);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handlePost(HttpExchange httpExchange) throws IOException {
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+
         URI uri = httpExchange.getRequestURI();
         String[] parsedUri = parseUri(uri.toString());
-        System.out.println(uri);
+        // Check if cookie already exists and if it's UUID is contained by sessionPool
 
-        if(method.equals("GET")) {
-            response = chooseProperPage(parsedUri);
-        }
+        if (cookieStr == null) {
+            redirectToLocation(httpExchange, "/html/index.html");
+        } else {
 
-        if(method.equals("POST")) {
-
-            if(parsedUri.length == 4 && parsedUri[subPageIndex].equals("store")) {
+            if(parsedUri.length == 3 && parsedUri[2].equals("store")) {
 
                 Artifact artifact = new Artifact("New artifact", "Artifact description", 10, false);
                 try {
@@ -54,7 +65,7 @@ public class MentorController implements HttpHandler {
                 }
             }
 
-            if(parsedUri.length == 4 && parsedUri[subPageIndex].equals("quest")) {
+            if(parsedUri.length == 3 && parsedUri[2].equals("quest")) {
                 Quest quest = new Quest("New quest", "Quest description", 10, false);
                 try {
                     questDao.addQuest(quest);
@@ -64,83 +75,62 @@ public class MentorController implements HttpHandler {
             }
         }
 
-        httpExchange.sendResponseHeaders(200, response.length());
-        OutputStream os = httpExchange.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
-
     }
 
-    private String chooseProperPage(String[] parsedUri){
-        String response="";
+    private void handleSession(HttpExchange httpExchange) throws IOException, SQLException {
+        URI uri = httpExchange.getRequestURI();
 
-        try {
-            if (parsedUri.length == 3 && parsedUri[mainPageIndex].equals("mentor")) {
-                response = getMentorMainPage(parsedUri[idIndex]);
+        String[] parsedUri = parseUri(uri.toString());
+        String response = chooseProperPage(parsedUri, httpExchange);
+        OutputStream os = httpExchange.getResponseBody();
+
+        if (response == null) {
+            // Object does not exist or is not a file: reject with 404 error.
+            send404(httpExchange);
+        } else {
+            // Object exists and is a file: accept with response code 200.
+            sendReq(httpExchange, response);
+        }
+
+        os.close();
+    }
+
+    private String chooseProperPage(String[] parsedUri, HttpExchange httpExchange) throws SQLException {
+        String response = "";
+        int subPageUri = 2;
+        int subPageUriLength = 3;
+        String login = getLoginByCookie(httpExchange);
+        Mentor mentor = getMentorByLogin(login);
+
+        if (parsedUri.length == subPageUriLength) {
+
+
+            if(parsedUri[subPageUri].equals("profile")){
+                response = renderPage(mentor, "templates/mentorProfile.twig");
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("profile")) {
-                response = getMentorProfile(parsedUri[idIndex]);
+            else if(parsedUri[subPageUri].equals("contact")){
+                response = renderPage(mentor, "templates/mentorContact.twig");
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("contact")){
-                response = getMentorContact(parsedUri[idIndex]);
-            }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("store")){
+            else if(parsedUri[subPageUri].equals("store")){
                 response = getMentorStore();
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("quest")){
+            else if(parsedUri[subPageUri].equals("quest")){
                 response = getMentorQuest();
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("inventory")){
-                response = getMentorRoom(parsedUri[idIndex]);
+            else if(parsedUri[subPageUri].equals("codecooler")){
+                    response = getMentorCodecoolerList(mentor);
             }
-            else if (parsedUri.length == 4 && parsedUri[subPageIndex].equals("codecooler")){
-                response = getMentorCodecoolerList(parsedUri[idIndex]);
-            }
-        } catch (SQLException e){
-            e.printStackTrace();
+
+        }else {
+            response = renderPage(mentor, "templates/mentorMainPage.twig");
         }
-        System.out.println(response);
+
         return response;
     }
 
-
-    private String getMentorMainPage(String id) throws SQLException{
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentorMainPage.twig");
-        JtwigModel model = JtwigModel.newModel();
-
-        MentorDAO mentorDAO = new MentorDAO();
-        Mentor mentor = mentorDAO.getMentor(String.valueOf(id));
-        model.with("mentor", mentor);
-
-        return template.render(model);
-    }
-
-    private String getMentorProfile(String id) throws SQLException{
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentorProfile.twig");
-        JtwigModel model = JtwigModel.newModel();
-
-        MentorDAO mentorDAO = new MentorDAO();
-        Mentor mentor = mentorDAO.getMentor(id);
-        model.with("mentor", mentor);
-
-        return template.render(model);
-    }
-
-    private String getMentorContact(String id) throws SQLException{
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentorContact.twig");
-        JtwigModel model = JtwigModel.newModel();
-
-        MentorDAO mentorDAO = new MentorDAO();
-        Mentor mentor = mentorDAO.getMentor(String.valueOf(id));
-        model.with("mentor", mentor);
-
-        return template.render(model);
-    }
-
-    private String getMentorStore() throws SQLException{
+    private String getMentorStore() throws SQLException {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentorStore.twig");
         JtwigModel model = JtwigModel.newModel();
-
         List<List> listOfLists = artefactDao.getNestedArtifactList(artefactDao.getListOfArtifact());
         model.with("artifacts", listOfLists);
 
@@ -157,31 +147,20 @@ public class MentorController implements HttpHandler {
         return template.render(model);
     }
 
-    private String getMentorRoom(String id) throws SQLException{
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentorRoom.twig");
-        JtwigModel model = JtwigModel.newModel();
-
-        MentorDAO mentorDAO = new MentorDAO();
-        Mentor mentor = mentorDAO.getMentor(String.valueOf(id));
-        model.with("mentor", mentor);
-
-        return template.render(model);
-    }
-
-    private String getMentorCodecoolerList(String id) throws SQLException {
+    private String getMentorCodecoolerList(Mentor mentor) throws SQLException {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentorCodecoolers.twig");
         JtwigModel model = JtwigModel.newModel();
 
-
-        Mentor mentor = mentorDao.getMentor(String.valueOf(id));
         List<Codecooler> codecoolerList = codecoolerDao.getListOfCodecoolers();
 
         model.with("mentor", mentor);
         model.with("studentList", codecoolerList);
 
         return template.render(model);
-
-
     }
 
+    private Mentor getMentorByLogin(String login) throws SQLException {
+        MentorDAO mentorDAO = new MentorDAO();
+        return mentorDAO.getMentor(login);
+    }
 }
